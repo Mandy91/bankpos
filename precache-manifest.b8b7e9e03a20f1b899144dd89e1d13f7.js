e0e19713bc8af92b89a45468cb8fc7ce2b7b3320
Mandeep Yadav <yadavmandy91@gmail.com>
2023-09-21 03:45:06 +0530

    Updates

---
+++ precache-manifest.b8b7e9e03a20f1b899144dd89e1d13f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "647b93e1df45749677e2",
+    "url": "/bankpos/static/css/main.9ffed260.chunk.css"
+  },
+  {
+    "revision": "647b93e1df45749677e2",
+    "url": "/bankpos/static/js/main.647b93e1.chunk.js"
+  },
+  {
+    "revision": "f09d6da400d0c14a7f1d",
+    "url": "/bankpos/static/js/1.f09d6da4.chunk.js"
+  },
+  {
+    "revision": "8f9066711fc72fee7d28",
+    "url": "/bankpos/static/js/2.8f906671.chunk.js"
+  },
+  {
+    "revision": "894bcf856f4a875e0cc3",
+    "url": "/bankpos/static/js/runtime~main.894bcf85.js"
+  },
+  {
+    "revision": "2104de3694e426da4922cad0debc0278",
+    "url": "/bankpos/index.html"
+  }
+];
